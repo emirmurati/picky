1475c20509abfb2233b974dca2f6f1971d0f4458
Emir Murati <emir_810@icloud.com>
2024-08-15 18:55:32 +0200

    redirect to profile on mobile version

--- luna-frontend/src/components/Header.jsx
+++ luna-frontend/src/components/Header.jsx
@@ -144,7 +144,7 @@ function Header() {
                 </Link>
                 <Link
                   onClick={() => setMobileMenuOpen(false)}
-                  to="/profile"
+                  to={token ? "/profile" : "login"}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Profile
